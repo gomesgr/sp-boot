5c239ce2865c008a635c789f878b115d96d0843d
Gabriel <gomes.g.r@outlook.com>
2020-11-16 14:26:22 -0300

    a

--- src/main/java/com/example/demoproject/aprendendo/AprendendoApplication.java
+++ src/main/java/com/example/demoproject/aprendendo/AprendendoApplication.java
@@ -2,6 +2,8 @@ package com.example.demoproject.aprendendo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AprendendoApplication {
@@ -10,4 +12,12 @@ public class AprendendoApplication {
 		SpringApplication.run(AprendendoApplication.class, args);
 	}
 
+}
+
+@RestController
+class Hello {
+	@GetMapping("/")
+	String hello() {
+		return "Hello";
+	}
 }
